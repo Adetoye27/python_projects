4bf08ca1d26842d807c7c8a27c15ea4f473b2b64
Adetoye Abodunrin <60989736+Adetoye27@users.noreply.github.com>
2025-03-26 20:03:20 -0500

    Update odd_even.py

--- odd_even.py
+++ odd_even.py
@@ -1,13 +1,17 @@
-#write a program  that will ask a user for a number then check whether that number is EVEN or odd
-#Display on the screen:
-#Please enter a number between 1 and 100
-#Your number user_number is even/odd
+# Write a program that will ask a user for a number 
+# then check whether that number is EVEN or ODD
+# Display on the screen:
+# Please enter a number between 1 and 100
+# Your number user_number is even/odd
 
-user_number = int(input("Please enter a number betwwn 1 and 100 \n"))
+user_number = int(input("Please enter a number between 1 and 100:\n"))
 
-if (user_number >= 1 and user_number <= 100) and (user_number % 2 ==0 ):
-    print(f"Your number {user_number} is even")
-elif (user_number >= 1 and user_number <= 100) and (user_number % 2 != 0):
-    print(f"Your number {user_number} is odd")
+if 1 <= user_number <= 100:
+    if user_number % 2 == 0:
+        print(f"Your number {user_number} is even")
+    else:
+        print(f"Your number {user_number} is odd")
 else:
     print(f"ERROR: Your number {user_number} is out of range")
+
+# Ensuring a newline at the end of the file
